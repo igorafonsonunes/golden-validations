69c2c8f4c6aafd44eef214de3f9ff697af5c43a2
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2021-03-05 22:43:33 -0300

    feat: create method for remove mask of cpf

--- src/cpf-validation.ts
+++ src/cpf-validation.ts
@@ -1,5 +1,6 @@
 export function validateCpf(cpf: string): boolean {
     if (cpf) {
+        cpf = removeMask(cpf);
         const cpfBroked: Array<number> = cpf.split('').map(x => +x);
         let countValidation: number = cpfBroked[0] * 10 + cpfBroked[1] * 9 + cpfBroked[2] * 8
             + cpfBroked[3] * 7 + cpfBroked[4] * 6 + cpfBroked[5] * 5 + cpfBroked[6] * 4
@@ -10,9 +11,9 @@ export function validateCpf(cpf: string): boolean {
             countValidation = cpfBroked[0] * 11 + cpfBroked[1] * 10 + cpfBroked[2] * 9
                 + cpfBroked[3] * 8 + cpfBroked[4] * 7 + cpfBroked[5] * 6 + cpfBroked[6] * 5
                 + cpfBroked[7] * 4 + cpfBroked[8] * 3 + cpfBroked[9] * 2;
-            
-            restOfDivision = countValidation*10 % 11;
-            if(restOfDivision === 10 && 0 === cpfBroked[10]){
+
+            restOfDivision = countValidation * 10 % 11;
+            if (restOfDivision === 10 && 0 === cpfBroked[10]) {
                 return true;
             }
             return false;
@@ -22,4 +23,14 @@ export function validateCpf(cpf: string): boolean {
         }
     }
     return false;
+}
+
+function removeMask(cpf: string): string {
+    if (cpf.includes('.') && cpf.includes('-')) {
+        if (cpf.split('.').length === 3 && cpf.split('-').length === 2) {
+            cpf = cpf.split('.')[0] + cpf.split('.')[1] + cpf.split('.')[2];
+            cpf = cpf.split('-')[0] + cpf.split('-')[1];
+        }
+    }
+    return cpf;
 }
--- src/test/cpf-validation.spec.ts
+++ src/test/cpf-validation.spec.ts
@@ -1,9 +1,39 @@
 import { validateCpf } from '../cpf-validation';
 
 describe('Testing CPFs', () =>{
-    test('shold return true for this cpf', ()=>{
+    test('shold return true for this cpf without mask', ()=>{
         const cpf = '57723285060'; 
         const validation = validateCpf(cpf);
         expect(validation).toBeTruthy();
-    })
+    });
+
+    test('shold return true for this cpf with mask', ()=>{
+        const cpf = '577.232.850-60'; 
+        const validation = validateCpf(cpf);
+        expect(validation).toBeTruthy();
+    });
+
+    test('shold return false for this cpf without mask', ()=>{
+        const cpf = '68135841060'; 
+        const validation = validateCpf(cpf);
+        expect(validation).toBeFalsy();
+    });
+
+    test('shold return false for this cpf with mask', ()=>{
+        const cpf = '681.358.410-60'; 
+        const validation = validateCpf(cpf);
+        expect(validation).toBeFalsy();
+    });
+
+    test('shold return false for this cpf without all numbers', ()=>{
+        const cpf = '68141060'; 
+        const validation = validateCpf(cpf);
+        expect(validation).toBeFalsy();
+    });
+
+    test('shold return false for this cpf with the wrong mask', ()=>{
+        const cpf = '577232.850-60'; 
+        const validation = validateCpf(cpf);
+        expect(validation).toBeFalsy();
+    });
 })
